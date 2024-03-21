d8a5f44373f2162989c56ec53ff3b97680b534e0
unknown <bobbebe40@gmail.com>
2024-03-21 11:43:30 +0800

    frontend folder structure and components setup

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <body>
+        <h1>Хэлний сонголт буруу байна.</h1>
+      </body>
+    </div>
+  );
+};
+
+export default NotFound;
