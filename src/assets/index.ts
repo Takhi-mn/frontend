c6c9cd2073fa80fb01dc76fb71d274e83561bb39
BIlguun B <145326196+bil9uun@users.noreply.github.com>
2024-03-22 14:11:00 +0800

    re-intro-test-design (#12)

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,27 @@
 import logo from "@/assets/logo.png";
-export { logo };
+import one from "@/assets/re-introduction/one.jpg";
+import two from "@/assets/re-introduction/two.jpg";
+import three from "@/assets/re-introduction/three.jpg";
+import four from "@/assets/re-introduction/four.png";
+import five from "@/assets/re-introduction/five.jpg";
+import six from "@/assets/re-introduction/six.png";
+import horseIcon from "@/assets/re-introduction/horse-icon.png";
+import coll1 from "@/assets/re-introduction/collection/coll1.jpg";
+import coll2 from "@/assets/re-introduction/collection/coll2.jpg";
+import coll3 from "@/assets/re-introduction/collection/coll3.jpg";
+import coll4 from "@/assets/re-introduction/collection/coll4.jpg";
+
+export {
+  logo,
+  one,
+  two,
+  three,
+  four,
+  coll1,
+  coll2,
+  coll3,
+  coll4,
+  five,
+  six,
+  horseIcon,
+};
