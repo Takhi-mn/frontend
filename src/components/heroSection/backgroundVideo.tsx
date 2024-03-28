63cdc21cb6c1a375324ff6b798df78d2c38b8b9e
rimzss <bobbebe40@gmail.com>
2024-03-28 21:29:31 +0800

    new video new me

--- src/components/heroSection/backgroundVideo.tsx
+++ src/components/heroSection/backgroundVideo.tsx
@@ -1,13 +1,16 @@
-import Video from "next-video";
 import React from "react";
-import zebra from "https://8ugqtxyj43pdrjam.public.blob.vercel-storage.com/zebra-czb7YmDF9y53s4qtaP0fXDeZOvAOxh.mp4";
 
 type Props = {};
 
 const BackgroundVideo = (props: Props) => {
   return (
     <div className="w-[1920px] absolute z-0 -left-[700px] sm:-left-[600px] md:-left-[500px] lg:-left-[400px] xl:-left-[300px] 2xl:left-0">
-      <Video autoPlay muted loop playsInline controls={false} src={zebra} />
+      <video autoPlay muted loop>
+        <source
+          src=" http://localhost:3000/videos/khomyn.mp4"
+          type="video/mp4"
+        />
+      </video>
     </div>
   );
 };
