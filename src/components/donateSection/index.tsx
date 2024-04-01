a945a3df59c5c8025ea34e2de1b8b5d03f78d96b
rimzss <bobbebe40@gmail.com>
2024-04-01 20:12:29 +0800

    donation section responsive fix

--- src/components/donateSection/index.tsx
+++ src/components/donateSection/index.tsx
@@ -11,12 +11,12 @@ type Props = {
 
 const DonateSection = ({ donate, today, donateText, once, monthly }: Props) => {
   return (
-    <section className="w-full px-10 py-6 md:p-16 md:px-28 md:h-[800px] md:flex gap-10 text-white bg-workPageBackground bg-center bg-cover">
-      <div className="flex-1 md:w-full md:h-full md:flex flex-col justify-center">
-        <h2 className="text-center text-3xl md:text-5xl font-medium">
+    <section className="w-full px-10 py-6 lg:p-16 lg:px-28 lg:h-[800px] lg:flex gap-10 text-white bg-workPageBackground bg-center bg-cover">
+      <div className="flex-1 lg:w-full lg:h-full lg:flex flex-col justify-center">
+        <h2 className="text-center text-3xl lg:text-5xl font-medium">
           {today}
         </h2>
-        <p className="mt-10 md:text-2xl font-light">{donateText}</p>
+        <p className="mt-10 lg:text-2xl font-light">{donateText}</p>
       </div>
       <DonateCard donate={donate} once={once} monthly={monthly} />
     </section>
